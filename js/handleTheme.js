0608eca7d470f8f23a33b12b05ed79978b061154
Cristhian <cristhianfernandolp@gmail.com>
2024-06-12 09:13:51 -0300

    feat: Function to toggle between dark and light mode

---
+++ js/handleTheme.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  let themeToggleBtns = document.querySelectorAll("button[data-theme]");
+
+  themeToggleBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const theme = this.dataset.theme;
+
+      if (theme === "light") {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else if (theme === "dark") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
+    });
+  });
+});
