880e79d57d2598dfa3e43372b2d5d072a5c38713
Cristhian <cristhianfernandolp@gmail.com>
2024-06-12 09:14:15 -0300

    style: CSS bundle and tailwind.config.js config to accept the dark mode toggle with classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}", "node_modules/preline/dist/*.js"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
