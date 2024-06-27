988f9ffb0a294ea5790c8de6c2135de49d6aea63
Cristhian <cristhianfernandolp@gmail.com>
2024-06-27 17:04:10 -0300

    Resolving

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-module.exports = {
-  content: ["./**/*.{html,js}", "node_modules/preline/dist/*.js"],
-=======
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
->>>>>>> vite-config
   darkMode: "class",
   theme: {
     extend: {
@@ -24,19 +19,6 @@ export default {
           900: "#42307D",
         },
         gray: {
-<<<<<<< HEAD
-          25: "#FCFCFD",
-          50: "#F9FAFB",
-          100: "#F2F4F7",
-          200: "#EAECF0",
-          300: "#D0D5DD",
-          400: "#98A2B3",
-          500: "#667085",
-          600: "#475467",
-          700: "#344054",
-          800: "#1D2939",
-          900: "#101828",
-=======
           50: "var(--gray-50)",
           100: "var(--gray-100)",
           200: "var(--gray-200)",
@@ -48,7 +30,6 @@ export default {
           800: "var(--gray-800)",
           900: "var(--gray-900)",
           950: "var(--gray-950)",
->>>>>>> vite-config
         },
       },
     },
