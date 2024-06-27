cf1c806acd53931a9bf4fd815a98020f5f148f5d
Cristhian <cristhianfernandolp@gmail.com>
2024-06-27 16:09:28 -0300

    chore: Removing documentation from project

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ import { initDatePicker } from "./src/js/date-picker";
 import { initFinalUrl } from "./src/js/final-url";
 import { initFormObject } from "./src/js/form-object";
 import { handleTheme } from "./src/js/handleTheme";
-import { initDocumentation } from "./src/js/how-it-works";
 import { initMoneyInputs } from "./src/js/money-inputs";
 import { initNewParameters } from "./src/js/new-parameter";
 import { initTooltips } from "./src/js/tooltip";
 
+import "flatpickr/dist/flatpickr.min.css";
+
 import hljs from "highlight.js";
+import "highlight.js/styles/github-dark.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   initServiceInput();
@@ -16,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
   initFinalUrl();
   initFormObject();
   handleTheme();
-  initDocumentation();
   initMoneyInputs();
   initNewParameters();
   initTooltips();
