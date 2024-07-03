63048e9465e3fd333bcdfc56134a25f689374732
Cristhian <cristhianfernandolp@gmail.com>
2024-07-03 09:12:34 -0300

    feat: class to handle with clipboard in final url

---
+++ src/js/multi-form/clipboardManager.js
@@ -0,0 +1,42 @@
+// clipboardManager.js
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+
+export class ClipboardManager {
+  constructor(buttonSelector, urlInputSelector) {
+    this.copyUrlButton = document.querySelector(buttonSelector);
+    this.finalUrlInput = document.querySelector(urlInputSelector);
+
+    this.init();
+  }
+
+  init() {
+    this.copyUrlButton.addEventListener("click", (e) =>
+      this.handleCopyClick(e)
+    );
+  }
+
+  handleCopyClick(e) {
+    e.preventDefault();
+    navigator.clipboard.writeText(this.finalUrlInput.getAttribute("value"));
+    this.showToast();
+  }
+
+  showToast() {
+    Toastify({
+      text: "Proposta copiada para sua area de transferência",
+      duration: 3000,
+      hideProgressBar: false,
+      progress: 3000,
+      gravity: "bottom",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "#262626",
+        border: "1px solid #373737",
+        borderRadius: "6px",
+        color: "#fafafa",
+      },
+    }).showToast();
+  }
+}
