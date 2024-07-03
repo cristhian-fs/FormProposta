b3eeccf8f2b4284fdfe63e88e7946946f362ea0b
Cristhian <cristhianfernandolp@gmail.com>
2024-07-03 09:11:57 -0300

    feat: module to handle with money inputs in multi-form

---
+++ src/js/multi-form/money-inputs.js
@@ -0,0 +1,107 @@
+export function PriceInputsMultiForm() {
+  const multiForm = document.querySelector("#multi-form");
+  const currencyInputs = multiForm.querySelectorAll("[data-price]");
+  const currencyOptions = multiForm.querySelectorAll(".currencyInput");
+  const prefixOptionsText = multiForm.querySelectorAll(".prefixCurrency");
+
+  let activeCurrency = "BRL";
+  const prefixCurrencyOptions = {
+    BRL: "R$",
+    USD: "$",
+    EUR: "€",
+  };
+
+  function updateCurrencyOptions(selectedCurrency) {
+    currencyOptions.forEach((option) => {
+      option.value = selectedCurrency;
+    });
+    prefixOptionsText.forEach((text) => {
+      text.textContent = prefixCurrencyOptions[selectedCurrency];
+    });
+  }
+
+  function formatNumberToCurrency(value) {
+    let formattedNumber;
+    switch (activeCurrency.toUpperCase()) {
+      case "BRL":
+        formattedNumber = new Intl.NumberFormat("pt-BR").format(value);
+        break;
+      case "USD":
+        formattedNumber = new Intl.NumberFormat("en-US").format(value);
+        break;
+      case "EUR":
+        formattedNumber = new Intl.NumberFormat("de-DE").format(value);
+        break;
+      default:
+        formattedNumber = "Unsupported currency";
+    }
+    return formattedNumber;
+  }
+
+  function updateInputValue(input) {
+    if (input.value) {
+      const unformattedValue = input.getAttribute("data-value");
+      input.value = formatNumberToCurrency(parseFloat(unformattedValue));
+    }
+  }
+
+  function handleCurrencyChange(event) {
+    activeCurrency = event.target.value;
+    updateCurrencyOptions(activeCurrency);
+
+    currencyInputs.forEach((input) => {
+      updateInputValue(input);
+    });
+  }
+
+  function handleInput(event) {
+    const input = event.target;
+    const unformattedValue = input.value.replace(/[^\d]/g, "");
+    const numericValue = parseInt(unformattedValue, 10);
+
+    if (!isNaN(numericValue)) {
+      input.value = formatNumberToCurrency(numericValue);
+      input.setAttribute("data-value", numericValue);
+
+      updateTotalPrice();
+    } else {
+      input.value = "";
+      input.setAttribute("data-value", "");
+    }
+  }
+
+  function updateTotalPrice() {
+    const totalPriceElement = document.querySelector("#preco-total");
+    const price = parseInt(
+      document.querySelector("#preco").getAttribute("data-value"),
+      10
+    );
+    const discount = parseInt(
+      document.querySelector("#desconto").getAttribute("data-value"),
+      10
+    );
+
+    if (!isNaN(price) && !isNaN(discount)) {
+      const totalPriceValue = price - discount;
+      totalPriceElement.setAttribute(
+        "value",
+        formatNumberToCurrency(totalPriceValue)
+      );
+      totalPriceElement.setAttribute("data-value", totalPriceValue);
+
+      const totalPriceEvent = new Event("input", { bubbles: true });
+      totalPriceElement.dispatchEvent(totalPriceEvent);
+    } else {
+      totalPriceElement.setAttribute("value", "");
+      totalPriceElement.setAttribute("data-value", "");
+    }
+  }
+
+  // Adicionar evento para alteração de moeda
+  currencyOptions[0].addEventListener("input", handleCurrencyChange);
+
+  // Adicionar eventos para os inputs de preço e desconto
+  currencyInputs.forEach((input) => {
+    input.addEventListener("input", handleInput);
+  });
+}
